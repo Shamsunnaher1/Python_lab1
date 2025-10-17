dbd23baf37991197c4ac00397ce067f221071151
Shuchona <shamsunnahershuchona@gmail.com>
2025-10-18 00:11:44 +0600

    7th commit

--- practice.py
+++ practice.py
@@ -78,10 +78,101 @@ print(int(a), int(b))
 #Operators
 
 #Write a program to check whether a number is even or odd using both modulo and bitwise operators
+#Modulo
 a = input("enter a number:")
 if int(a) % 2 == 0:
     print("number is even")
 else:
     print("number is odd")
     
-g = 100
+
+#bitwise
+if int(a) & 1 == 0:
+    print("number is even")
+else:
+    print("number is odd")
+
+
+#Write a program to find the maximum of three numbers using logical operators.
+a = 12
+b = 60
+c = 9
+if a > b and a > c:
+    print("a is maximum")
+elif b > a and b > c:
+    print("b is maximum")
+elif c > a and c > b:
+    print("c is maximum")
+
+
+#Write a program to convert total days into years, weeks, and remaining days.
+days = input("enter total days: ")
+years = int(days) / 365
+r = int(days) % 365
+weeks = int(r) / 7
+rd = int(r) % 7
+print("years: ", int(years), "weeks: ", int(weeks), "remaining days: ", int(rd)) 
+
+
+#Write a program to find the minimum of two numbers using the conditional (ternary) operator.
+a = 5
+b = 10
+if  a < b:
+    print("a is minnimum")
+else:
+    print("b is minnimum")
+
+
+#Write a program to demonstrate the behavior of pre-increment and post-increment operators.
+a = 10
+# pre increment
+a = a + 2
+b = a
+print("pre increment value b: ", int(b))
+print("pre increment value a: ", int(a))
+
+a = 10
+#post increment
+b = a
+a = a + 2
+print("post increment value b: ", int(b))
+print("post increment value a: ", int(a))
+
+
+#Decision Making
+
+#Write a program to check whether a number is positive, negative, or neutral.
+a = input("enter a number: ")
+if int(a) > 0:
+     print("number is positive")
+elif int(a) < 0:
+     print("number is negative")
+else:
+     print("number is neutral")
+
+
+#Write a program to check whether a given year is a leap year or not.
+year = input("enter the year: ")
+if (int(year) % 4 == 0 and int(year) % 100 != 0) or int(year) % 400 == 0:
+    print("year is leap year")
+else:
+    print("not leap year")
+
+
+#Write a program to find the grade of a student based on marks.
+marks = input("enter marks: ")
+if int(marks) >= 80:
+    print("A+")
+elif int(marks) >= 70 and int(marks) < 80:
+    print("A")
+elif int(marks) >= 60 and int(marks) < 70:
+    print("A-")
+elif int(marks) >= 50 and int(marks) < 60:
+    print("B") 
+elif int(marks) >= 40 and int(marks) < 50:
+    print("C")
+else:
+    print("fail")
+
+
+#Write a program to determine whether a character is a vowel, consonant, digit, or special symbol.
